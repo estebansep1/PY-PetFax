6088c24e88d24a6a89323d4ea4f0ba73b4dbd693
Esteban Sepulveda <tebansd@icloud.com>
2023-12-10 11:28:55 -0800

    implemented flask in different file

--- app.py
+++ app.py
@@ -1,14 +1,2 @@
-# Config
-from flask import Flask
-app = Flask(__name__)
-
-# Index Route
-@app.route('/')
-def index():
-    return 'Hello, this is PetFax'
-
-
-# Pets Index Routes
-@app.route('/pets')
-def pets():
-    return 'These are our pets available for adoption!'
+from petfax import create_app
+app = create_app()
